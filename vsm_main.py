8ad28f60a3a00eb9db5f76d53750f86df2699b6b
zishuo <zishuo.wung@gmail.com>
2024-06-23 01:07:29 -0700

    Added extensive enhancements to vsm_main.py
    - Introduced `is_audio_or_video_file` to identify valid media files.
    - Created `transcribe_and_save` function to handle transcription and saving of files.
    - Refined `download_youtube_transcript` to improve robustness against missing transcripts.
    - Unified downloading logic in `download_youtube_video` for audio and video streams.
    - Integrated Whisper AI (`transcribe_audio`) for more accurate audio transcription.
    - Incorporated OpenAI GPT model for text summarization within `openai_summarize_text`.
    - Added Ollama API support via `ollama_summarize` function for alternative summarization.
    - Implemented `traverse_and_transcribe` to batch process files in directories.
    - Enhanced command-line interface, parsing multiple flags and adding `print_usage` for guidance.
    - Ensured modular code structure for better readability and maintainability.

--- vsm_main.py
+++ vsm_main.py
@@ -1,170 +1,262 @@
 #!/usr/bin/env python3
 import sys
+import os
 import subprocess
-import whisper
-from openai import OpenAI
 from pytube import YouTube
+from whisper import available_models, load_model
 from whisper.utils import get_writer
+from openai import OpenAI
+import ollama
+
+
+def is_audio_or_video_file(file_name):
+    """Determines if a given file is an audio or video file based on its extension."""
+    audio_video_extensions = {'.mp3', '.wav', '.mp4', '.m4a', '.flv', '.avi', '.mov', '.wmv', '.mkv'}
+    _, ext = os.path.splitext(file_name)
+    return ext.lower() in audio_video_extensions
+
+
+
+def transcribe_and_save(file_path, url=None, title=None, description=None):
+    """Transcribe a single audio/video file and save the transcript to disk."""
+    path = os.path.dirname(file_path)
+    filename = os.path.basename(file_path)
+    try:
+        result = transcribe_audio(file_path)
+        text = result["text"]
+        print(f"Transcription complete:\n{text}")
+        transcription_file = f"{file_path}.txt"
+        with open(transcription_file, "w", encoding='utf-8') as file:
+            if(url):
+                file.write(f"URL: {url}\n")
+            if(title):
+                file.write(f"Title: {title}\n")
+            if(description):
+                file.write(f"Description: {description}\n")
+            file.write(text)
+        print(f"Saved transcription to {transcription_file}")
+
+        #save to srt file:
+        srt_writer = get_writer("srt", path)
+        srt_name = f"{filename}"
+        print(f"Saved srt to {path}/{srt_name}")
+        srt_writer(result, srt_name)
+
+        return text
+
+    except Exception as e:
+        print(f"An error occurred while transcribing the file: {e}")
+
+
+
 
 
 def download_youtube_transcript(url, path):
-    #download transcript
-    video = YouTube(url)
-    l_list = video.captions.all()
-    print(l_list)
-    print(l_list[0].code)
-    transcript = video.captions.get_by_language_code(l_list[0].code)
-    if transcript:
-        # Generate SRT captions
+    """Download the transcript of a YouTube video in SRT format."""
+    try:
+        video = YouTube(url)
+        transcript_list = video.captions.all()
+        if not transcript_list:
+            raise ValueError("No transcripts found")
+
+        transcript = video.captions.get_by_language_code(transcript_list[0].code)
+        if not transcript:
+            raise ValueError("Transcript not found for the specified language")
+
         transcript_srt = transcript.generate_srt_captions()
-        # Write the transcript to a file
-        with open(f"{path}/{video.title}.srt", 'w') as f:
+        file_path = f"{path}/{video.title.replace(' ', '_')}.srt"
+        with open(file_path, 'w') as f:
             f.write(transcript_srt)
-        # with open(f"{path}/{video.title}.xml", 'w') as f:
-        #     f.write(transcript.xml_captions)
-        print("Audio and transcript downloaded successfully!")
-        return f"{path}/{video.title}.srt"
-    else:
-        print("Transcript not found for the specified language.")
+        print("Transcript downloaded successfully!")
+        return file_path
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return None
 
-
-
-def download_youtube_video(url, path, v=False):
+def download_youtube_video(url, path, video_mode=False):
+    """Download the video or audio from a YouTube URL."""
     try:
+        video = YouTube(url)
+        print(f"Title: {video.title}")
+        print(f"Description: {video.description}")
 
-        # # Create a YouTube object
-        clip = YouTube(url)
-        print(clip.title)
-        print(clip.description)
-        name = ""
-        # # Get the video with the highest resolution
-        if v:
-            print("Downloading the best video")
-            video_hr = clip.streams.get_highest_resolution()
-
-        # # Set the output path and filename
-            name = f"{clip.publish_date.date()}-{clip.title}.mp4"
-            name = name.replace(" ", "_")
-        # # Download the video to the specified path and filename
-            video_hr.download(path, name)
-            print("Video downloaded successfully!")
-
+        file_name = f"{video.publish_date.date()}-{video.title.replace(' ', '_')}"
+        if video_mode:
+            stream = video.streams.get_highest_resolution()
+            file_extension = "mp4"
         else:
-            print("Downloading the audio")
-            audio = clip.streams.get_audio_only()
-            # Set the output path and filename,include the title of the video and the date
-            name = f"{clip.publish_date.date()}-{clip.title}.wav"
-            #replace the spaces with underscores
-            name = name.replace(" ", "_")
-            audio.download(path, name)
-            print("Audio downloaded successfully!")
+            stream = video.streams.get_audio_only()
+            file_extension = "wav"
+
+        final_path = f"{path}/{file_name}.{file_extension}"
+        stream.download(path, f"{file_name}.{file_extension}")
+        print(f"{'Video' if video_mode else 'Audio'} downloaded successfully!")
+
+        return video.title, video.description, final_path
 
     except Exception as e:
         print(f"An error occurred: {e}")
-
-    return clip.title, clip.description, f"{path}/{name}"
-
-
+        return None, None, None
 
 def transcribe_audio(audio_file_path):
+    """Transcribe audio using Whisper AI."""
+    try:
+        print("Transcribing the audio file...")
+        model = load_model("large-v2")
+        result = model.transcribe(audio_file_path, language="en", verbose=True)
+        return result
 
-    #use whisper to transcribe the audio
-    print("Transcribing the audio file")
-    print(audio_file_path)
-    print(whisper.available_models())
-    model = whisper.load_model("large-v2")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
-    #save the transcribed text to a file
-    #save the tts to a file
-    #set language to English
-    result = model.transcribe(audio_file_path, language="en", verbose=True)
+def openai_summarize_text(text, api_key):
+    """Summarize the text using OpenAI GPT."""
+    try:
+        client = OpenAI(api_key=api_key)
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": "Make a detailed takeaway of the text."},
+                {"role": "user", "content": text}
+            ],
+            temperature=1,
+            max_tokens=4096
+        )
+        return response.choices[0].message.content
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+#call ollama api to summary text
+def ollama_chat_print(summary):
+    print(f"Model: {summary['model']}")
+    print(f"Created At: {summary['created_at']}")
+    print(f"Message Role: {summary['message']['role']}")
+    print(f"Content: \n{summary['message']['content']}")
+    print(f"Done Reason: {summary['done_reason']}")
+    print(f"Done: {summary['done']}")
+    print(f"Total Duration: {summary['total_duration']}")
+    print(f"Load Duration: {summary['load_duration']}")
+    print(f"Prompt Eval Count: {summary['prompt_eval_count']}")
+    print(f"Prompt Eval Duration: {summary['prompt_eval_duration']}")
+    print(f"Eval Count: {summary['eval_count']}")
+    print(f"Eval Duration: {summary['eval_duration']}")
 
 
-    return result
-
-def summarize_text(text):
-    #the text to summarize could be very very long, so we need to split it into multiple parts
-    #and then summarize each part
-    #use chat gpt to summarize the text
-    client = OpenAI(api_key="sk-proj-ly9CAM2lghMAUhXzvvOgT3BlbkFJfzajVO4fdUqQk2wJ2HRH")
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": "Make a detailed takeaway of the text in  original language, probably English or Chinese."},
+def ollama_summarize(text):
+    """Summarize text using the Ollama API."""
+    prompt = "Summarize the following text into detailed key long takeaways. it also contains important info and a insight at the end: "
+    chat_url = "http://localhost:11434/api/chat"
+    query_url = "http://localhost:11434/api/generate"
+    model = "llama3-gradient:latest"
+    payload = {
+        "model": model,
+        "messages": [
+            {"role": "system", "content": prompt},
             {"role": "user", "content": text}
         ],
-        temperature=1,
-        max_tokens=4096,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-        )
-    return response.choices[0].message.content
+        "stream": False
+    }
+
+    query = {
+        "model": "llama3-gradient:latest",
+        "prompt": prompt + text,
+        "options": {
+            "num_ctx": 16000
+        }
+    }
+
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = ollama.generate(model=model, prompt=prompt+text)
+    return response['response']
+
+def traverse_and_transcribe(root_path):
+    """Walk through all subfolders and transcribe audio/video files."""
+
+
+    for subdir, _, files in os.walk(root_path):
+        for file in files:
+            if is_audio_or_video_file(file):
+                print(f"Found audio/video file: {file}")
+
+                # Construct full file path
+                file_path = os.path.join(subdir, file)
+
+                # Transcribe file
+                text = transcribe_and_save(file_path)
+                #get OPEN AI key from os environment variable.
+                api_key = os.environ['OPENAI_API_KEY']
+                summary = openai_summarize_text(text, api_key)
+                with open(f"{file_path}.takeaway.txt",'w', encoding='utf-8') as file:
+                    file.write(summary)
+
+def print_usage():
+    """Prints the usage instructions for the script."""
+    print("Error in command line arguments.")
+    print("Usage:")
+    print("  python3 vsm_main.py -a <youtube-video-url> -p <path/to/save/file>")
+    print("  python3 vsm_main.py -v <youtube-video-id> -p <path/to/save/file>")
+    print("  python3 vsm_main.py -t <path/to/audio/file>")
+    print("  python3 vsm_main.py -f <file/to/summary>")
+    print("  python3 vsm_main.py -c <string/to/summary>")
+
+def main():
+    """Main function to parse command-line arguments and perform actions."""
+    args = sys.argv
+    url, path, text, audio_file_path, api_key = "", "./", "", "", "your-api-key"
+    title, description = "", ""
+    video_mode = False
+    root = ""
+    transcription_file = ""
+
+    try:
+        if "-a" in args:
+            url = args[args.index("-a")+1]
+        if "-v" in args:
+            url = args[args.index("-v")+1]
+            video_mode = True
+        if "-p" in args:
+            path = args[args.index("-p")+1]
+        if "-t" in args:
+            audio_file_path = args[args.index("-t")+1]
+        if "-c" in args:
+            text = args[args.index("-c")+1]
+        if "-f" in args:
+            print("open " + args[args.index("-f")+1])
+            with open(args[args.index("-f")+1], 'r') as file:
+                text = file.read()
+        if "-r" in args:
+            root = args[args.index("-r")+1]
+    except:
+        print_usage()
+
+
+
+    if url and path:
+        title, description, audio_file_path = download_youtube_video(url, path, video_mode)
+        print(f"Downloaded video to {path} and saved audio to {audio_file_path}")
+    if audio_file_path:
+        text = transcribe_and_save(audio_file_path,url,title,description)
+    if text:
+        #get OPEN AI key from os environment variable.
+        api_key = os.environ['OPENAI_API_KEY']
+        summary = openai_summarize_text(text, api_key)
+        #summary = ollama_summarize(text)
+        print(summary)
+        if transcription_file:
+            with open(f"{transcription_file}.takeaway.txt",'w', encoding='utf-8') as file:
+                file.write(summary)
+    if root:
+        traverse_and_transcribe(root)
+    else:
+        print(f"No audio file path provided.")
+
+
 
 if __name__ == "__main__":
-    # Get the string from the command line arguments
-    #parse -a option to get the youtube video
-    #parse -c option to get the text from command line
-    #parse -f option to get the text from a file
-    #parse -t option to transcribe the audio
-    #parse -v option to get the video from the youtube
-    text = ""
-    path = "./"
-    audio_file_path = ""
-    url = ""
-    title = ""
-    desciption = ""
-    v = False
-
-    if "-a" in sys.argv:
-        url = sys.argv[sys.argv.index("-y")+1]
-    elif "-v" in sys.argv:
-        url = sys.argv[sys.argv.index("-v")+1]
-        v = True
-    elif "-p" in sys.argv:
-        path = sys.argv[sys.argv.index("-p")+1]
-    elif "-c" in sys.argv:
-        text = sys.argv[sys.argv.index("-c")+1]
-    elif "-f" in sys.argv:
-        with open(sys.argv[sys.argv.index("-f")+1], 'r') as f:
-            text = f.read()
-    elif "-t" in sys.argv:
-        audio_file_path = sys.argv[sys.argv.index("-t")+1]
-    else:
-        print(url)
-        print(audio_file_path)
-        print(path)
-        print(text)
-        sys.exit(1)
-    if url != "" and path != "":
-        # Call the download_youtube_transcript function
-        # the out put is the path to the transcript file and vedio title
-        title, desciption, audio_file_path = download_youtube_video(url, path, v)
-
-    if audio_file_path != "":
-        # Call the transcribe_audio function
-        result = transcribe_audio(audio_file_path)
-        text = result["text"]
-        print("Transcription complete!")
-        print(text)
-        #save to the file:
-        txt_name = f"{audio_file_path}.txt"
-        with open(txt_name, 'w', encoding='utf-8') as f:
-            f.write(url)
-            f.write(desciption)
-            f.write(text)
-        #save to srt file:
-        srt_writer = get_writer("srt", path)
-        srt_name = f"{audio_file_path}.srt"
-        srt_writer(result, srt_name)
-
-
-    if text != "":
-        # Call the summarize_text function
-        summary = summarize_text(text)
-            #save the summary to a file
-        with open(f"{audio_file_path}.takeaway.txt", 'w', encoding='utf-8') as f:
-            f.write(summary)
-        print("Summary complete!")
-        print(summary)
+    main()
